d264a271d6ac0174f9f3daa730a3354072a3691d
Alex Meek <am01919@surrey.ac.uk>
2025-05-11 22:01:16 +0100

    Improved util functions, using nvim from bash is dumb

--- install_nvim.sh
+++ install_nvim.sh
@@ -3,6 +3,7 @@
 # To Do
 
 # - [ ] Why is this better than just following the tutorial
+    # 1. Using on multiple systems e.g. remote servers. (need to make this script robust, i.e. if curl is missing etc.)
 # - [ ] Make it easer to change the configuration, e.g. the colorscheme.
 # - [ ] Make sure each line is necessary, and we don't have a tonne of duplicate code 
 # - [ ] Make sure the sections/layout makes sense.
@@ -12,60 +13,80 @@
 
 # Status - working, but still more plugins to install and remaps to configure. 
 
-
+# Stop script if any non-zero exit code is encountered.
 set -e
 
-# Functions
-nvim_create_file () {
+create_file () {
     local file="$1"
-    nvim --headless +"edit $file" +w +qa > /dev/null 2>&1
-    # Check if the file exists
-    if [[ ! -f "$file" ]]; then
-        echo "Failed to create file \"$file\" using nvim."
+    if ! echo "" > $file; then
+        echo "Failed to create file \"$file\"."
+        return 1
     else
         echo "File \"$file\" created successfully."
     fi
+    return 0
 }
 
-nvim_add_to_file() {
+delete_file () {
     local file="$1"
-    local edits="$2"
-
-    # Build Lua array from Bash multiline string
-    local lua_array=""
-    while IFS= read -r line; do
-        lua_array="$lua_array'${line//\'/\\\'}', "
-    done <<< "$edits"
-    lua_array="{${lua_array%, }}"
-
-    # Debug output (optional)
-    # echo "Lua array to pass: $lua_array"
-
-    # Append lines at the end using Neovim
-    nvim --headless +"edit $file" +"lua local last=vim.api.nvim_buf_line_count(0); vim.api.nvim_buf_set_lines(0, last, last, false, $lua_array)" +w +qa > /dev/null 2>&1
-
-    if [ $? -ne 0 ]; then
-        echo "Failed to add to file \"$file\" using nvim."
-    else
-        echo "File \"$file\" updated successfully."
+    if [[ -f "$file" ]]; then
+        if rm $file; then
+            echo "Failed to remove file \"$file\"."
+            return 1
+        fi
     fi
 }
 
-create_fresh_directory () {
+create_directory () {
+    local directory="$1"
+    if ! mkdir -p $directory; then
+        echo "Failed to create directory \"$directory\"."
+        return 1
+    else
+        echo "Directory \"$directory\" created successfully."
+    fi
+    return 0
+}
+
+delete_directory () {
     local directory="$1"
     if [[ -d $directory ]]; then
-        yes | rm -r $directory
+        if ! yes | rm -r $directory; then
+            echo "Failed to remove directory \"$directory\"."
+            return 1
+        fi
     fi
-    mkdir -p $directory
 }
 
 create_fresh_file () {
     local file="$1"
-    if [[ -f "$file" ]]; then
-        rm $file
-    fi
-    nvim_create_file $file
+    delete_file $file
+    create_file $file
+    return 0
 }
+
+create_fresh_directory () {
+    local directory="$1"
+    delete_directory $directory
+    create_directory $directory
+    return 0
+}
+
+add_to_file () {
+    local file="$1"
+    local edits="$2"
+
+    if ! { cat >> "$file" <<EOF
+$edits
+EOF
+    }; then
+        echo "Failed to append to file \"$file\"."
+        return 1
+    fi
+
+    return 0
+}
+
 
 
 # Install NeoVim
@@ -140,8 +161,8 @@ do
     create_fresh_file $i
 done
 
-nvim_add_to_file $SUB_LUA_FILEPATH "print(\"Hello, from $SUB_LUA_FILEPATH!\")"
-nvim_add_to_file $BASE_LUA_FILEPATH "require(\"$SUB_LUA_DIR_NAME\")"
+add_to_file $SUB_LUA_FILEPATH "print(\"Hello, from $SUB_LUA_FILEPATH!\")"
+add_to_file $BASE_LUA_FILEPATH "require(\"$SUB_LUA_DIR_NAME\")"
 
 ###############################################################################
 ###############################################################################
@@ -150,9 +171,9 @@ nvim_add_to_file $BASE_LUA_FILEPATH "require(\"$SUB_LUA_DIR_NAME\")"
 ###############################################################################
 ################################### Remaps ####################################
 ###############################################################################
-nvim_add_to_file $REMAP_FILEPATH "vim.g.mapleader = \" \""
-nvim_add_to_file $REMAP_FILEPATH "vim.keymap.set(\"n\", \"<leader>pv\", \":Ex<CR>\")"
-nvim_add_to_file $BASE_LUA_FILEPATH "require(\"$SUB_LUA_DIR_NAME.remap\")"
+add_to_file $REMAP_FILEPATH "vim.g.mapleader = \" \""
+add_to_file $REMAP_FILEPATH "vim.keymap.set(\"n\", \"<leader>pv\", \":Ex<CR>\")"
+add_to_file $BASE_LUA_FILEPATH "require(\"$SUB_LUA_DIR_NAME.remap\")"
 ###############################################################################
 ###############################################################################
 ###############################################################################
@@ -195,8 +216,8 @@ return require('packer').startup(function(use)
 
 end)"
 
-nvim_add_to_file $PACKER_FILEPATH "$CONTENT"
-nvim_add_to_file $BASE_LUA_FILEPATH "require(\"$SUB_LUA_DIR_NAME.packer\")"
+add_to_file $PACKER_FILEPATH "$CONTENT"
+add_to_file $BASE_LUA_FILEPATH "require(\"$SUB_LUA_DIR_NAME.packer\")"
 
 nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync"
 ###############################################################################
@@ -206,15 +227,15 @@ nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync"
 ###############################################################################
 ################################ Telescope install ############################
 ###############################################################################
-nvim_add_to_file $TELESCOPE_FILEPATH "local builtin = require('telescope.builtin')"
-nvim_add_to_file $TELESCOPE_FILEPATH "vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })"
-nvim_add_to_file $TELESCOPE_FILEPATH "vim.keymap.set('n', '<C-p>', builtin.git_files, { })"
+add_to_file $TELESCOPE_FILEPATH "local builtin = require('telescope.builtin')"
+add_to_file $TELESCOPE_FILEPATH "vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })"
+add_to_file $TELESCOPE_FILEPATH "vim.keymap.set('n', '<C-p>', builtin.git_files, { })"
 
 CONTENT="vim.keymap.set('n', '<leader>ps', function() 
     builtin.grep_string({ search = vim.fn.input(\"Grep > \") });
 end)"
 
-nvim_add_to_file $TELESCOPE_FILEPATH "$CONTENT"
+add_to_file $TELESCOPE_FILEPATH "$CONTENT"
 ###############################################################################
 ###############################################################################
 ###############################################################################
@@ -248,7 +269,7 @@ vim.api.nvim_create_autocmd(\"ColorScheme\", {
 ColourMyPencils()
 "
 
-nvim_add_to_file $COLOURS_LUA_FILEPATH "$CONTENT"
+add_to_file $COLOURS_LUA_FILEPATH "$CONTENT"
 
 ###############################################################################
 ###############################################################################
@@ -313,9 +334,7 @@ require'nvim-treesitter.configs'.setup {
 }
 "
 
-cat <<EOF > "$TREESITTER_LUA_FILEPATH"
-$CONTENT
-EOF
+add_to_file $TREESITTER_LUA_FILEPATH "$CONTENT"
 ###############################################################################
 ###############################################################################
 ###############################################################################
